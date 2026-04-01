1b555bedb2ee47f3dcf4d720db2b4fa0e4897bcf
Pastor <viruszold@gmail.com>
2026-04-02 00:52:54 +0300

    v34

--- grammar/.output/elevator_main.c
+++ grammar/.output/elevator_main.c
@@ -3,35 +3,44 @@
 struct Ports {
 };
 
-static void port_write_bit(const int address, const int bit, const bool val, void *userdata) {
+static void port_write_bit(int address, int bit, bool val, void *userdata) {
+    struct Ports *ports = (struct Ports *) userdata;
     (void) address;
     (void) bit;
     (void) val;
-    (void) userdata;
+    (void) ports;
 }
 
-static bool port_read_bit(const int address, const int bit, void *userdata) {
+static bool port_read_bit(int address, int bit, void *userdata) {
+    struct Ports *ports = (struct Ports *) userdata;
     (void) address;
     (void) bit;
-    (void) userdata;
+    (void) ports;
     return false;
 }
-static void port_write_float(const int address, const int bit, const float val, void *userdata) {
+static void port_write_float(int address, int bit, float val, void *userdata) {
+    struct Ports *ports = (struct Ports *) userdata;
     (void) address;
     (void) bit;
     (void) val;
-    (void) userdata;
+    (void) ports;
 }
-static float port_read_float(const int address, const int bit, void *userdata) {
+static float port_read_float(int address, int bit, void *userdata) {
+    struct Ports *ports = (struct Ports *) userdata;
     (void) address;
     (void) bit;
-    (void) userdata;
+    (void) ports;
     return 0.0f;
 }
 
 int main(void) {
     struct Ports ports = {};
-    struct Elevator elevator = {.userdata = &ports, .write_bit = port_write_bit, .read_bit = port_read_bit, .write_float = port_write_float, .read_float = port_read_float};
+    struct Elevator elevator = {
+            .userdata = &ports,
+            .write_bit = port_write_bit,
+            .read_bit = port_read_bit,
+            .write_float = port_write_float,
+            .read_float = port_read_float};
 
     Elevator_init(&elevator);
     while (!Elevator_is_done(&elevator)) {
